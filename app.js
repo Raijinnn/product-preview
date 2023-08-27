828c2e92eea6584deabbad2f55076953f56ccb4e
Airus Alcantara <91376645+Raijinnn@users.noreply.github.com>
2023-08-27 18:25:08 +0800

    still making the email logic

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const  form = document.getElementById("form");
+const form = document.getElementById("form");
+const input = document.getElementById("email");
+const span = document.getElementById("email-failed");
+const container = document.getElementById("")
 
-form.addEventListener('submit', (e)=>{
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-
-
-})
+    if (!input.value) {
+        span.classList.add('span-validation')
+        input.classList.add('input-fail')
+        console.log(input.value)
+    } else {
+        span.classList.remove('span-validation');
+        input.classList.remove('input-fail')
+        console.log(input.value)
+    }
+});
